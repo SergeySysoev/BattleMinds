baab6e077ff96447cc3d9e51a41c6388df176b0d
Sergey Sysoev <sergey.sysoev@decartel.co>
2024-11-12 17:30:09 +0100

    Remove unused external content, update User profile with Steam info

--- Source/BattleMinds/BattleMinds.Build.cs
+++ Source/BattleMinds/BattleMinds.Build.cs
@@ -10,7 +10,7 @@ public class BattleMinds : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AdvancedSessions" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AdvancedSessions", "OnlineSubsystemSteam" });
 
 		PublicIncludePaths.AddRange(new string[]
 		{
